aea2d407b77f3c6c6bd57d9c18b1c75da7c819ff
Serhii <1SsaibotT1@gmail.com>
2018-11-22 15:32:42 +0200

    Project setup with cocoaPods

---
+++ Github Gists Editor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Gists Editor
+//
+//  Created by Serhii on 11/22/18.
+//  Copyright © 2018 Serhii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
